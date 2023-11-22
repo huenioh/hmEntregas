223ae3b17526b681eeaa6ba923082f266a62e422
HuenioHugo <hueniohugomoura@gmail.com>
2023-11-21 21:09:11 -0300

    resolvido erro com a splashView

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,8 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:hm_entregas/modules/home/home_temp.dart';
-import 'package:hm_entregas/modules/login/login_temp.dart';
+import 'package:hm_entregas/modules/home/page.dart';
 import 'package:hm_entregas/modules/login/page.dart';
-import 'package:hm_entregas/modules/splash/page.dart';
-
 import 'package:firebase_core/firebase_core.dart';
+import 'package:hm_entregas/modules/splash/page.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,6 +15,7 @@ void main() async {
 
 var routes = <String, WidgetBuilder>{
   '/login': (BuildContext context) => const LoginScreen(),
+  '/home': (BuildContext context) => const HomeScreen(),
 };
 
 class MyApp extends StatelessWidget {
@@ -27,27 +25,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const RoteadorTela(),
+      home: const SplashView(),
       routes: routes,
-    );
-  }
-}
-
-//Para manter o usuario logado
-class RoteadorTela extends StatelessWidget {
-  const RoteadorTela({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return StreamBuilder<User?>(
-      stream: FirebaseAuth.instance.userChanges(),
-      builder: (context, snapshot) {
-        if (snapshot.hasData) {
-          return const Home();
-        } else {
-          return Login();
-        }
-      },
     );
   }
 }
--- lib/modules/splash/page.dart
+++ lib/modules/splash/page.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:hm_entregas/modules/home/home_temp.dart';
+import 'package:hm_entregas/modules/login/login_temp.dart';
 import 'package:hm_entregas/modules/splash/componentes/child.dart';
 
 class SplashView extends StatefulWidget {
@@ -14,11 +17,33 @@ class _SplashViewState extends State<SplashView> {
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 2), () {
-      Navigator.pushReplacementNamed(context, '/login');
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const RoteadorTela()),
+      );
     });
   }
 
   Widget build(BuildContext context) {
     return const MyWidget();
+  }
+}
+
+//Para manter o usuario logado
+class RoteadorTela extends StatelessWidget {
+  const RoteadorTela({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<User?>(
+      stream: FirebaseAuth.instance.userChanges(),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          return const Home();
+        } else {
+          return Login();
+        }
+      },
+    );
   }
 }
